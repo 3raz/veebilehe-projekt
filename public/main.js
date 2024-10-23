692127930452b49ceb93cb9b7cf2fa32ed9c4afb
smokeytube2 <test106437@gmail.com>
2024-10-23 16:52:51 +0300

    kaamera

--- public/main.js
+++ public/main.js
@@ -7,16 +7,38 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 
+
+
 // Create a renderer
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+  });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
 
 // Create a geometry and a material, then combine them into a mesh
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+
+function moveCamera() {
+    const t = document.body.getBoundingClientRect().top;
+
+    camera.position.z += t * -0.001;
+    camera.position.x += t * -0.0005;
+    camera.position.y += t * -0.01;
+    camera.rotation.y += t * -0.0015;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
+
 
 // Animation loop
 function animate() {
