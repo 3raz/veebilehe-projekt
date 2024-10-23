c234dd14ee4010d80bec8653952c7c59d57d483d
smokeytube2 <test106437@gmail.com>
2024-10-23 18:03:36 +0300

    Update main.js

--- public/main.js
+++ public/main.js
@@ -4,9 +4,9 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 
 // Create a camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.001, 100000);
 camera.rotation.x = -3.1415926535/2;
-camera.position.y = 10;
+camera.position.y = 0.01;
 
 
 
@@ -17,12 +17,17 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(ambientLight);
-
-const gridHelper = new THREE.GridHelper(200, 50);
+const gridHelper = new THREE.GridHelper(2000, 50);
 scene.add(gridHelper)
+
+const gridHelper2 = new THREE.GridHelper(200, 50);
+scene.add(gridHelper2)
+
+const gridHelper3 = new THREE.GridHelper(20, 50);
+scene.add(gridHelper3)
+
+const gridHelper4 = new THREE.GridHelper(2, 50);
+scene.add(gridHelper4)
 
 // Create a geometry and a material, then combine them into a mesh
 const geometry = new THREE.BoxGeometry();
@@ -31,27 +36,58 @@ const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 var t = 1;
-function moveCamera() {
-    //const t = document.body.getBoundingClientRect().top;
 
-    //camera.position.y += Math.log(t * 0.001);
-    camera.position.y += Math.log(camera.position.y/9.9999999);
-    console.log(camera.position.y)
+
+let moveSpeed = 0.0000000001;
+let direction = {
+    ArrowUp: false,
+    ArrowDown: false,
+};
+
+function moveCamera() {
+    if (direction.ArrowUp) camera.position.y -= 10**(-3 + (camera.position.y));
+    if (direction.ArrowDown) camera.position.y += 10**(-3 + (camera.position.y));
+
+    if (camera.position.y > 150) {
+        scene.remove(gridHelper3);
+    }
+    if (camera.position.y > 800) {
+        scene.remove(gridHelper2);
+    }
+
+    console.log(10**(camera.position.y * moveSpeed))
 }
 
-document.body.onscroll = moveCamera;
-moveCamera();
+
+window.addEventListener('keydown', (event) => {
+    direction[event.key] = true;
+});
+
+window.addEventListener('keyup', (event) => {
+    direction[event.key] = false;
+});
+
+
 
 
 // Animation loop
 function animate() {
     requestAnimationFrame(animate);
 
-    // Rotate the cube
+    moveCamera();
+
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
 
     renderer.render(scene, camera);
+
 }
 
 animate();
+
+
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+});
